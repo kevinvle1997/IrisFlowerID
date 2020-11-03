4a024fc8d136e4d6e499c748a31d7117764357b2
kevinvle1997 <kevinvle1997@gmail.com>
2020-11-02 22:59:10 -0600

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # testML
 Getting Acquainted with ML
+
+Used https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
--- main.py
+++ main.py
@@ -72,3 +72,5 @@ predictions = model.predict(X_validation)
 print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
+
+
